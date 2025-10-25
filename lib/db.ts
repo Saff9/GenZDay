22361e93229a492fc4aa36057edc903fc1cf4140
Saff9 <saffanakbar942@gmail.com>
2025-10-25 21:26:13 +0530

    Delete lib directory

--- lib/db.ts
+++
@@ -1,7 +0,0 @@
-// Simple Xata client setup
-import { XataClient } from './xata';
-
-export const xata = new XataClient({
-  apiKey: process.env.XATA_API_KEY,
-  databaseURL: process.env.XATA_DATABASE_URL,
-});
